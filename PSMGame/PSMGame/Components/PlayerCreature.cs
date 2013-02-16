b5094b33540e1150a489dcc65cb87a3090809326
Arthur Ward <ssbfalcon@gmail.com>
2013-02-16 15:52:32 -0500

    Moved creature changes to components dir

--- PSMGame/PSMGame/Components/PlayerCreature.cs
+++ PSMGame/PSMGame/Components/PlayerCreature.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Sce.PlayStation.Core;
 using Sce.PlayStation.HighLevel.GameEngine2D;
 using Sce.PlayStation.HighLevel.GameEngine2D.Base;
@@ -7,15 +8,33 @@ namespace PSM
 {
 	public class PlayerCreature
 	{
-		public SpriteUV sprite;
+		public SpriteTile sprite;
 		private TextureInfo texInfo;
-		
+		public Animation CurrentAnimation;
+
+		public Dictionary<string, Animation> Animations; 
+
 		public PlayerCreature ()
 		{
-			texInfo = new TextureInfo ("/Application/assets/square.png");
-			sprite = new SpriteUV (texInfo);
+			Animations = new Dictionary<string, Animation>();
+			texInfo = new TextureInfo(AssetManager.GetTexture("spritesheet"), new Vector2i(2,2), TRS.Quad0_1);
+			Animations.Add("idle" , new Animation(0, 2, 0.4f, false));
+			CurrentAnimation = Animations["idle"];
+			CurrentAnimation.Play();
+			sprite = new SpriteTile(texInfo);
+			sprite.TileIndex1D = CurrentAnimation.CurrentFrame;
 			sprite.Quad.S = texInfo.TextureSizef;
+		}
+
+		public void SetAnimation(string animation)
+		{
+			CurrentAnimation = Animations[animation];	
+		}
+
+		public void Update(float dt)
+		{
+			CurrentAnimation.Update(dt);
+			sprite.TileIndex1D = CurrentAnimation.CurrentFrame;
 		}
 	}
 }
-
